940a4db03b0c10a6d2008b8f8fbb9746b639a7e4
ARPITSHARMA24 <113551635+ARPITSHARMA24@users.noreply.github.com>
2025-02-05 21:27:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,18 +76,18 @@ let trackList = [
         path: "./music/only.mp3",
     },
     {
-        name: "Day & Night",
-        artist: "Jung Seung Hwan",
+        name: "Piya Ghar Aayenge",
+        artist: "Kailash Kher",
         path: "./music/day and night.mp3",
     },
     {
-        name: "Love of my Life",
-        artist: "Reyne",
+        name: "Chand Si Mehbooba",
+        artist: "Md. Rafi",
         path: "./music/love of my life.mp3",
-    },
+    };
     {
-        name: "The Only One",
-        artist: "Reyne",
+        name: "Oh Rangrez",
+        artist: "Javed Bashir",
         path: "./music/the only one.mp3",
     },
 ];
