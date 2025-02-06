845ef3e2328206fd2b7ad4329d3fbdd204ba9b46
ARPITSHARMA24 <113551635+ARPITSHARMA24@users.noreply.github.com>
2025-02-06 09:23:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,17 +75,17 @@ let trackList = [
     {
         name: "Piya Ghar Aayenge",
         artist: "Kailash Kher",
-        path: "./music/day and night.mp3",
+        path: "./music/day_and_night.mp3",
     },
     {
         name: "Chand Si Mehbooba",
         artist: "Md. Rafi",
-        path: "./music/love of my life.mp3",
+        path: "./music/love_of_my_life.mp3",
     },
     {
         name: "Oh Rangrez",
         artist: "Javed Bashir",
-        path: "./music/the only one.mp3",
+        path: "./music/the_only_one.mp3",
     }
 ];
 
